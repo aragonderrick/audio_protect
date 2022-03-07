effc87414c36a167863474a7cac16e9474a5d874
aragonderrick <38571434+aragonderrick@users.noreply.github.com>
2022-03-07 09:32:22 -0800

    Update MainComponent.h

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,20 +2,9 @@
 
 #include <JuceHeader.h>
 
-class MainComponent  : public juce::AudioAppComponent, public juce::ChangeListener, private juce::Timer
+class MainComponent  : public juce::AudioAppComponent, private juce::Timer
 {
 public:
-    // Enumerations
-    enum TransportState
-    {
-        Stopped,    //Audio playback is stopped and ready to be started.
-        Starting,   //Audio playback hasn't yet started but it has been told to start.
-        Pausing,    //Audio is pausing.
-        Paused,     //Audio playback is paused and ready to be started.
-        Playing,    //Audio is playing.
-        Stopping    //Audio is playing but playback has been told to stop, after this it will return to the Stopped state.
-    };
-
     enum
     {
         fftOrder = 14,
@@ -29,31 +18,24 @@ public:
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+    //==============================================================================
     void openButtonClicked();
-    void playButtonClicked();
-    void stopButtonClicked();
-    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
-    void changeState(TransportState newState);
     void timerCallback() override; //repaints at 60 hz when playing the wav file
-
     void pushNextSampleIntoFifo(float sample) noexcept;
     void drawNextLineOfSpectrogram();
     void readInFileFFT(const juce::File& file);
     void drawSpectrogram();
-    void paint (juce::Graphics& g) override;
-    void resized() override;
 
 private:
     // Buttons
     juce::TextButton openButton;
-    juce::TextButton playButton;
-    juce::TextButton stopButton;
 
     // Format and Audio Variables
     juce::AudioFormatManager formatManager; // takes care of audio formatting
     std::unique_ptr< juce::AudioFormatReaderSource> readerSource; // source to read audio data from
     juce::AudioTransportSource transportSource; // allows audio to be played, stopped, etc
-    TransportState current_state;
     std::unique_ptr<juce::FileChooser> chooser;
     juce::AudioSampleBuffer fileBuffer; // stores the data from the file
 
@@ -70,12 +52,11 @@ private:
     int fifoIndex = 0;
     bool nextFFTBlockReady = false;
     float maxValue;
-    //double sampleRate;
     float** channels;
     int position; //position in the array of sample data
     juce::String currentSizeAsString;
     juce::String currentStatus;
-    //Image myimage = ImageFileFormat::loadFrom(File("thefullpathtothefile.gif"));
     juce::Image logo = juce::ImageFileFormat::loadFrom(juce::File("C:/Users/arago/OneDrive/Desktop/Spring2022/CSCI490/images/logo.png"));
+   
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
