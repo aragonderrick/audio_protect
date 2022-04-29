32d6785e818bb03feac20543830b267630815495
aragonderrick <38571434+aragonderrick@users.noreply.github.com>
2022-04-28 23:09:04 -0700

    Update hashTable.h

--- Source/hashTable.h
+++ Source/hashTable.h
@@ -5,8 +5,35 @@
     Created: 11 Apr 2022 11:51:31pm
     Author:  arago
 
+    The following code was changed and adapted from the follwoing tutorial:
+    https://www.educative.io/edpresso/how-to-implement-a-hash-table-in-cpp
+
   ==============================================================================
 */
 
 #pragma once
+#include <JuceHeader.h>
+#include "DataPoint.h"
+#include <iostream>
+#include <list>
+#include <map>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+class HashTable {
+public:
+    // Constructor to create a hash table with 'n' indices:
+    HashTable(int n);
+
+    // Insert data in the hash table:
+    void insertElement(long fp, int time, std::string name);
+
+    void printAll();
+
+private:
+    //list<long>* table;
+    std::map<long, std::vector<DataPoint>> table;
+
+};
