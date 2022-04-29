b617b022a4e7fa50ded32f3ccee05142df2cf0ef
aragonderrick <38571434+aragonderrick@users.noreply.github.com>
2022-04-28 23:09:31 -0700

    Update hashTable.h

--- Source/hashTable.h
+++ Source/hashTable.h
@@ -1,39 +1,55 @@
 /*
   ==============================================================================
 
-    hashTable.h
+    hashTable.cpp
     Created: 11 Apr 2022 11:51:31pm
     Author:  arago
-
-    The following code was changed and adapted from the follwoing tutorial:
-    https://www.educative.io/edpresso/how-to-implement-a-hash-table-in-cpp
 
   ==============================================================================
 */
 
-#pragma once
-#include <JuceHeader.h>
-#include "DataPoint.h"
-#include <iostream>
-#include <list>
-#include <map>
-#include <vector>
-#include <string>
+#include "hashTable.h"
+// Constructor to create a hash table with 'n' indices:
+HashTable::HashTable(int n) {
 
-using namespace std;
+    // Do nothing for now
 
-class HashTable {
-public:
-    // Constructor to create a hash table with 'n' indices:
-    HashTable(int n);
+}// end HashTable()
 
-    // Insert data in the hash table:
-    void insertElement(long fp, int time, std::string name);
+// Insert data in the hash table:
+void HashTable::insertElement(long fp, int time, std::string name) {
+    // insert the song name and time into the table/map
+    // check if fingerprint exists
+    //if (table[fp].size()) {
+    //    // make sure its not the same file
+    //    for (int i = 0; i < (int)table[fp].size(); i++) {
+    //        if (name == table[fp][i].getSongId()) {
+    //            // if it is then add it 
+    //            table[fp].push_back(DataPoint(name, time));
+    //        }
+    //        else {
+    //            DBG("\n==== WARNING SONG EXISTS ====\n");
+    //            table[fp].push_back(DataPoint(name, time));
+    //        }
+    //    }
+    //}
+    //else {
+    //    table[fp].push_back(DataPoint(name, time));
+    //}
+     table[fp].push_back(DataPoint(name, time));
 
-    void printAll();
+}// end insertElement()
 
-private:
-    //list<long>* table;
-    std::map<long, std::vector<DataPoint>> table;
-
-};
+void HashTable::printAll() {
+    DBG("NUMBER OF FINGER PRINTS: " << table.size());
+    for (auto const& [key, val] : table) {
+        //DBG("*\n" << key);
+        //DBG(val.size());
+        DBG("\nFingerprint: " << key);
+        DBG("Songs with this finger print: ");
+        for (auto dp : val) {
+            DBG(dp.getSongId() << " " << dp.getTime() << ",");
+            //DBG(dp.getSongId() << " " << dp.getTime());
+        }
+    }
+}// end printAll()
