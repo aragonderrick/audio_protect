d04e28e3ea8c0ccbd00e27596a23aa8747a3a7f9
aragonderrick <38571434+aragonderrick@users.noreply.github.com>
2022-05-01 18:04:23 -0700

    Update MainComponent.cpp

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,13 +1,15 @@
 #include "MainComponent.h"
 #include <sstream>
+#include <algorithm>
 
 //==============================================================================
 MainComponent::MainComponent()
     :
-    openButton("Select a File..."), 
-    fft(fftOrder), 
-    spectrogramImage(juce::Image::RGB, 660, 330, true), 
-    constellationImage(juce::Image::RGB, 660, 330, true), 
+    openButton("Fingerprint a New File"),
+    checkButton("Audio Protect an Existing File"),
+    fft(fftOrder),
+    spectrogramImage(juce::Image::RGB, 660, 330, true),
+    constellationImage(juce::Image::RGB, 660, 330, true),
     combinedImage(juce::Image::RGB, 1360, 330, true),
     maxValue(1),
     hashtable(3)
@@ -15,12 +17,16 @@ MainComponent::MainComponent()
     // Buttons
     addAndMakeVisible(&openButton);
     openButton.onClick = [this] { openButtonClicked(); };
+    addAndMakeVisible(&checkButton);
+    checkButton.onClick = [this] { checkButtonClicked(); };
+    //addAndMakeVisible(&checkButton);
+    //openButton.onClick = [this] { checkButtonClicked(); };
 
     setOpaque(true);
 
     setAudioChannels(1, 0);
 
-    setSize (1400, 900);
+    setSize(1400, 900);
 
     formatManager.registerBasicFormats(); // allows for .wav and .aaif files
 
@@ -33,13 +39,13 @@ MainComponent::~MainComponent() {
 }
 
 //==============================================================================
-void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate) {
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate) {
     // no audio play back
     // set range for data normalization
-    normalRange = makeRange::withCentre(float((double(fftSize) / sampleRate) * 20.f), float((double(fftSize) / sampleRate) * 20000.f), float((double(fftSize)/sampleRate)*1000.f));
+    normalRange = makeRange::withCentre(float((double(fftSize) / sampleRate) * 20.f), float((double(fftSize) / sampleRate) * 20000.f), float((double(fftSize) / sampleRate) * 1000.f));
 }
 
-void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) {
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) {
     // no audio play back
     bufferToFill.clearActiveBufferRegion();
 }
@@ -49,24 +55,24 @@ void MainComponent::releaseResources() {
 }
 
 //==============================================================================
-void MainComponent::paint (juce::Graphics& g) {
+void MainComponent::paint(juce::Graphics& g) {
     // background color
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
     // three main images
     g.drawImageAt(spectrogramImage, 20, 20);
-    g.drawImageAt(constellationImage, (getWidth() / 2)+20, 20);
+    g.drawImageAt(constellationImage, (getWidth() / 2) + 20, 20);
     g.drawImageAt(combinedImage, 20, 380);
     // logo
-    g.drawImageAt(logo, (getWidth() / 2)-160, 750);
+    g.drawImageAt(logo, (getWidth() / 2) - 160, 750);
     // title bars
-    g.setColour(juce::Colour (27, 27, 27));
+    g.setColour(juce::Colour(27, 27, 27));
     g.fillRect(20, 20, 660, 30);
     g.fillRect((getWidth() / 2) + 20, 20, 660, 30);
     g.fillRect(20, 380, 1360, 30);
     // title bar text
     g.setFont(20.0f);
     g.setColour(juce::Colours::white);
-    g.drawMultiLineText(currentStatus, (getWidth()-400), 800, 400);
+    g.drawMultiLineText(currentStatus, (getWidth() - 440), 800, 400);
     g.drawText(currentSizeAsString, getLocalBounds(), juce::Justification::bottomRight, true);
     g.drawMultiLineText("Spectrogram No Peaks", 270, 40, 400);
     g.drawMultiLineText("Peaks No Spectrogram", (getWidth() / 2) + 270, 40, 400);
@@ -74,7 +80,8 @@ void MainComponent::paint (juce::Graphics& g) {
 }
 
 void MainComponent::resized() {
-    openButton.setBounds(100, 780, ((getWidth() - 20)/2)-320, 30);
+    openButton.setBounds(100, 760, ((getWidth() - 20) / 2) - 320, 30);
+    checkButton.setBounds(100, 800, ((getWidth() - 20) / 2) - 320, 30);
     currentSizeAsString = juce::String(getWidth()) + " x " + juce::String(getHeight());
 }// end resize()
 
@@ -84,10 +91,10 @@ void MainComponent::populateFingerprints() {
     //juce::Array<juce::File> wavFiles;
     //wavFiles = audioFiles.findChildFiles(2, false, "*.wav");
     //for (int i = 0; i < (int)wavFiles.size(); i++) {
-    //   readInFileFFT(wavFiles[i]);
+    //    readInFileFFT(wavFiles[i]);
     //}
-    draw = true;
-    const juce::File fingerprintData("C:/Users/arago/OneDrive/Desktop/Spring2022/CSCI490/formated_database_small.txt");
+    //draw = true;
+    const juce::File fingerprintData("C:/Users/arago/OneDrive/Desktop/Spring2022/CSCI490/formated_database.txt");
     if (!fingerprintData.existsAsFile()) {
         DBG(fingerprintData.getFileName() << " doesnt not exist");
         return;  // file doesn't exist
@@ -98,7 +105,7 @@ void MainComponent::populateFingerprints() {
         return;  // failed to open
     }
     bool hasFingerprint = false;
-    while (!inputStream.isExhausted()){
+    while (!inputStream.isExhausted()) {
         std::istringstream ss(inputStream.readNextLine().toStdString());
         std::string word;
         long fp;
@@ -106,7 +113,7 @@ void MainComponent::populateFingerprints() {
         if (word == "*") {
             hasFingerprint = true;
         }
-        else if(hasFingerprint){
+        else if (hasFingerprint) {
             fp = std::stol(word);
             ss = (std::istringstream)inputStream.readNextLine().toStdString();
             ss >> word;
@@ -124,7 +131,7 @@ void MainComponent::populateFingerprints() {
             hasFingerprint = false;
         }
     }
-    hashtable.printAll();
+    //hashtable.printAll();
     currentStatus = "Populated the Database with 25 Songs.";
     //DBG(wavFiles.size());
 }// end populateFingerprints()
@@ -151,17 +158,17 @@ void MainComponent::drawNextLineOfSpectrogram() {
     fft.performFrequencyOnlyForwardTransform(fftData.data());
     auto maxLevel = juce::FloatVectorOperations::findMinAndMax(fftData.data(), fftSize / 2);
     // prepare vectors for push_back()
-    constellationData.resize(pixelX+1);
+    constellationData.resize(pixelX + 1);
     hashingData.resize(pixelX + 1);
     // for each pixel on the y-axis
     for (auto y = 1; y < imageHeight; ++y) {
         // normalize the data
         auto normalization = (float)y / imageHeight;
-        auto fftDataIndex = normalRange.convertFrom0to1((1-normalization));
+        auto fftDataIndex = normalRange.convertFrom0to1((1 - normalization));
         auto level = juce::jmap(fftData[fftDataIndex], 0.0f, juce::jmax(maxLevel.getEnd(), 1e-5f), 0.0f, 1.0f);
         // store key points
         hashingData[pixelX].push_back(std::make_pair(fftData[fftDataIndex], y)); // implicit caste to int for fftData[fftDataIndex]
-        constellationData[pixelX].push_back(std::make_pair(level,y));
+        constellationData[pixelX].push_back(std::make_pair(level, y));
         if (draw) {
             // draw the image
             spectrogramImage.setPixelAt(pixelX, y, juce::Colour::fromHSV(level, 1.0f, level, 1.0f));
@@ -189,11 +196,10 @@ void MainComponent::drawConstellationImage() {
     }
 }// end constellationImage()
 
-
 void MainComponent::generateFingerprint() {
     int frames_per_second = std::floor(hashingData.size() / duration);
     int seconds_passed = 0;
-    for (int x = 0; x < (int)hashingData.size(); x+= frames_per_second) {
+    for (int x = 0; x < (int)hashingData.size(); x += frames_per_second) {
         // Every 1 second in the data, I will only fingerprint these points
         // sort to get the most prominent points (peak frequency points)
         std::sort(hashingData[x].begin(), hashingData[x].end());
@@ -202,7 +208,7 @@ void MainComponent::generateFingerprint() {
         int i = 1;
         // GRAB UNIQUE VALUES from sorted hashingdata[], take the top 5 'strongest'/'robust' points
         while (peakPoints.size() < 5 && (hashingData[x].end() - i) != hashingData[x].begin()) {
-            int searchVal = (hashingData[x].end()- i)->first;
+            int searchVal = (hashingData[x].end() - i)->first;
             //DBG("Is " << searchVal << " in the vector?");
             if (std::find_if(peakPoints.begin(), peakPoints.end(), [searchVal](const auto& pair) { return pair.first == searchVal; }) == peakPoints.end()) {
                 // insert if it isnt already a peak point
@@ -213,12 +219,12 @@ void MainComponent::generateFingerprint() {
 
         // sort to get in order of increasing y value ... 0->1->2->3 which is from .second() of the pair
         std::sort(peakPoints.begin(), peakPoints.end(),
-        [](const std::pair<int, int>& x, const std::pair<int, int>& y)
+            [](const std::pair<int, int>& x, const std::pair<int, int>& y)
         {
             return x.second < y.second;
         });
 
-        // hash these peak frequency values in their correctn increasing order "hash a bin"
+        // hash these peak frequency values in their correct increasing order "hash a bin"
         std::hash<int> hasher;
         long fingerprint = 0;
         for (int y = 0; y < (int)peakPoints.size(); y++) {
@@ -226,14 +232,87 @@ void MainComponent::generateFingerprint() {
             fingerprint += hasher(peakPoints[y].second);
         }
         //DBG("fingerprint for pixelX = " << x << " is " << fingerprint);
-        hashtable.insertElement(fingerprint, seconds_passed, song_name);
+
+        if (draw) {
+            hashtable.insertElement(fingerprint, seconds_passed, song_name);
+        }
+        else {
+            std::vector<std::pair<std::string, int>> song_matches;
+            if (hashtable.check(fingerprint, seconds_passed, song_matches)) {
+                potential_matches.push_back(song_matches);
+            }
+        }
+
         fingerprint = 0;
         seconds_passed++;
     }
     //hashtable.printAll();
 }// end generateFingerprint()
 
+void MainComponent::makePrediction() {
+    std::map<int, std::vector<std::string>> matches;
+
+    for (int i = 0; i < potential_matches.size(); i++) {
+        for (int j = 0; j < potential_matches[i].size(); j++) {
+            matches[potential_matches[i][j].second].push_back(potential_matches[i][j].first);
+        }
+    }
+
+    std::string guestimate = "";
+    int guestimate_occurance = 1;
+
+    for (auto it = matches.begin(); it != matches.end(); it++) {
+        if (it->second.size() >= guestimate_occurance) {
+            std::map<string, int> occurence;
+            auto it2 = it;
+            for (int i = 0; i < 4 && it2 != matches.end(); it2++, i++) {
+                for (auto dp : it2->second) {
+                    occurence[dp]++;
+                }
+            }
+            
+            //it2 = it;
+            //if (it2 == matches.begin()) {
+            //    it2++;
+            //    it2++;
+            //    it2++;
+            //}
+            //for (int i = 0; i < 3; it2--, i++) {
+            //    for (auto dp : it2->second) {
+            //        occurence[dp]++;
+            //    }
+            //}
+
+            for (auto it3 : occurence) {
+                if (it3.second >= guestimate_occurance) {
+                    guestimate_occurance = it3.second;
+                    guestimate = it3.first;
+                }
+            }
+        }
+    }
+
+    //for (auto const& [key, val] : matches) {
+    //    if (val.size() >= guestimate_occurance) {
+    //        std::map<string, int> occurence;
+    //        for (auto dp : val) {
+    //            occurence[dp]++;
+    //        }
+    //        for (auto it : occurence) {
+    //            if (it.second >= guestimate_occurance) {
+    //                guestimate_occurance = it.second;
+    //                guestimate = it.first;
+    //            }
+    //        }
+    //    }
+    //}
+    currentStatus = "Detected " + guestimate;
+
+    //DBG(guestimate);
+}// end makePrediction()
+
 void MainComponent::openButtonClicked() {
+    draw = true;
     //Create the FileChooser object with a short message and allow the user to select only .wav files
     chooser = std::make_unique<juce::FileChooser>("Select a Wave file to play...", juce::File{}, "*.wav");
     // Pop up the FileChooser object
@@ -243,8 +322,19 @@ void MainComponent::openButtonClicked() {
     });
 }// openButtonClicked()
 
+void MainComponent::checkButtonClicked() {
+    draw = false;
+    //Create the FileChooser object with a short message and allow the user to select only .wav files
+    chooser = std::make_unique<juce::FileChooser>("Select a Wave file to check...", juce::File{}, "*.wav");
+    // Pop up the FileChooser object
+    auto chooserFlags = juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles;
+    chooser->launchAsync(chooserFlags, [this](const juce::FileChooser& fc) {
+        readInFileFFT(fc.getResult());
+    });
+}// openButtonClicked()
+
 void MainComponent::readInFileFFT(const juce::File& file) {
-    currentStatus = "Processing " + file.getFileName().toStdString();
+    currentStatus = "Fingerprinted " + file.getFileName().toStdString();
     song_name = file.getFileName().toStdString();
     if (file != juce::File{}) {
         auto* reader = formatManager.createReaderFor(file); //reads in from the chosen file
@@ -272,6 +362,8 @@ void MainComponent::drawSpectrogram() {
     std::fill(fftData.begin(), fftData.end(), 0.0f);
     std::fill(fifo.begin(), fifo.end(), 0.0f);
     fifoIndex = 0;
+    std::vector<std::vector<std::pair<std::string, int>>> new_one;
+    potential_matches = new_one;
 
     //clear the images
     spectrogramImage.clear(spectrogramImage.getBounds(), juce::Colours::black);
@@ -284,11 +376,14 @@ void MainComponent::drawSpectrogram() {
         if (nextFFTBlockReady) {
             drawNextLineOfSpectrogram();
             nextFFTBlockReady = false;
-            pixelX+=1;
+            pixelX += 1;
         }
         position++;
     }
     drawConstellationImage();
     generateFingerprint();
+    if (potential_matches.size() && !draw) {
+        makePrediction();
+    }
     repaint();
 }// drawSpectrogram()
